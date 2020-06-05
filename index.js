2f178a72d60635d9ab120d9c8c843d3e428ecf5a
Kat Yeary <katyeary@gmail.com>
2020-06-04 22:31:26 -0700

    commit function takes in md as argument

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
-//const axios = require('axios');
 const inquirer = require('inquirer');
 const util = require('util');
 const Octokat = require('octokat');
+//const template = require('./template.js');
 
 
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 
-function promptUser() {
+ function promptUser() {
   return inquirer
     .prompt([
       {
@@ -89,49 +89,47 @@ function generateMD(answers) {
   
   ${answers.tests}`
 
-}
+} 
 
-/* async function init() {
+ async function init() {
   try {
     const answers = await promptUser();
 
     const md = generateMD(answers);
-
-    await writeFileAsync("test.txt", md);
+    
+    await commitREADME(md);
+    await writeFileAsync("README.md", md);
     console.log("Successfully wrote to README.md");
   } catch(err) {
     console.log(err);
-  }
-}
- */
-
-//init();
-gitHub();
-
-   
-    function gitHub() {
-
+  } 
+ }
+init();
+  
+    function commitREADME(md) {
+      
         const octo = new Octokat ({
         username: "koyeary",
         password: "y3@Ry19520817"
       })
       const repo = octo.repos('koyeary', 'classic-richard-move');
-      let b = new Buffer("here's some words");
-      let s = b.toString("base64");
-     // const README = readFileAsync('README.md', 'base64');
+    
+      let b = new Buffer.from(md);
+      let str = b.toString("base64");
+
 
       repo.contents('README.md').fetch() 
       .then((info) => { 
         const sha = info.sha;
          const config = {
           message: 'Updating README.md File',
-          content: s,
+          content: str,
           encoding: "base64",
           sha: sha
         }
         repo.contents('README.md').add(config)
         .then((info) => {
-          console.log('File Updated. New sha is ', info.commit.sha) 
+          console.log('README.md file updated in git repo ${answers.repo}. New sha is ', info.commit.sha) 
         }).then(null, (err) => console.error(err));
       })
     }
